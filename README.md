e95bbd08d854e66192bd8db1a4f2a1c170f6eac6
likezheng <likezheng@fuse.co.id>
2023-12-06 14:58:59 +0800

    CS-0000: init & 规划

--- README.md
+++ README.md
@@ -3,3 +3,26 @@
 
 
 vue-json-schema
+formily
+vue
+
+
+参考：
+Demo: https://github.com/codon19/formily-vue
+
+
+阶段一：
+搭建工程
+
+可行性测试
+formily
+Vue.Draggable
+vue-json-schema-form
+monaco-editor ｜ json-editor-vue
+
+要求： 
+eslint代码风格问题
+demo-formily 
+nestjs 数据库支持（docker?）静态资源
+
+阶段二
--- packages/client/src/main.js
+++ packages/client/src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import VueCompositionAPI from '@vue/composition-api'
+import 'element-ui/lib/theme-chalk/index.css'
+import Element from 'element-ui'
+Vue.use(Element)
+
+// Vue.use(VueCompositionAPI)
 
 Vue.config.productionTip = false
 
